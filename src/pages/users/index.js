fc06b05ac83c44cdd5a84923027df6bb7101a528
Van Hiep <1712430@student.hcmus.edu.vn>
2020-12-20 00:44:24 +0700

    add ui list user hashcode

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Container, makeStyles } from '@material-ui/core';
+import Results from './results';
+import Toolbar from './toolbar';
+import data from './data';
 import UserDetails from './id';
-const Users = () => {
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3),
+  },
+}));
+
+const CustomerListView = () => {
+  const classes = useStyles();
+  const [customers] = useState(data);
+
   return (
-    <div>
-      <div>Users Page</div>
-    </div>
+    <Container maxWidth={false}>
+      <Toolbar />
+      <Box mt={3}>
+        <Results customers={customers} />
+      </Box>
+    </Container>
   );
 };
-export default Users;
+
+export default CustomerListView;
+
 export { UserDetails };
---
+++ src/pages/users/toolbar.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon,
+  makeStyles,
+} from '@material-ui/core';
+import { Search as SearchIcon } from 'react-feather';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(2),
+  },
+  importButton: {
+    marginRight: theme.spacing(1),
+  },
+  exportButton: {
+    marginRight: theme.spacing(1),
+  },
+  content: {
+    padding: theme.spacing(2),
+    '&:last-child': {
+      paddingBottom: theme.spacing(2),
+    },
+  },
+}));
+
+const Toolbar = ({ className, ...rest }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={clsx(classes.root, className)} {...rest}>
+      <Box display="flex" justifyContent="flex-end">
+        <Button color="primary" variant="contained">
+          Add user
+        </Button>
+      </Box>
+      <Box mt={3}>
+        <Card>
+          <CardContent className={classes.content}>
+            <Box maxWidth={500}>
+              <TextField
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SvgIcon fontSize="small" color="action">
+                        <SearchIcon />
+                      </SvgIcon>
+                    </InputAdornment>
+                  ),
+                }}
+                placeholder="Search customer"
+                variant="outlined"
+              />
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    </div>
+  );
+};
+
+Toolbar.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Toolbar;
