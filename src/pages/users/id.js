00adcbe89002d6deda32d8ad3166aeab39bf531c
Van Hiep <1712430@student.hcmus.edu.vn>
2020-12-27 13:34:56 +0700

    improve ui user details

--- src/pages/users/id.js
+++ src/pages/users/id.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles, useTheme, withStyles } from '@material-ui/core/styles';
 import {
   Paper,
   Grid,
   Card,
   CardHeader,
   CardActionArea,
-  CardActions,
+  Avatar,
   CardContent,
   CardMedia,
-  Button,
+  Badge,
   Typography,
   IconButton,
   Divider,
@@ -21,6 +21,38 @@ import {
   SkipPrevious as SkipPreviousIcon,
   PlayArrow as PlayArrowIcon,
 } from '@material-ui/icons';
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    width: 12,
+    height: 12,
+    borderRadius: '50%',
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: '$ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}))(Badge);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -68,6 +100,30 @@ const useStyles = makeStyles((theme) => ({
     height: 38,
     width: 38,
   },
+  contentHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    position: 'relative',
+    paddingBottom: theme.spacing(0),
+  },
+  rootAvatar: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+    position: 'absolute',
+    top: -95,
+  },
+  large: {
+    width: theme.spacing(22),
+    height: theme.spacing(22),
+  },
+  name: {
+    marginTop: theme.spacing(10),
+    fontWeight: 900,
+    fontFamily: 'Poppins',
+  },
 }));
 
 const UserDetails = () => {
@@ -79,35 +135,41 @@ const UserDetails = () => {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="https://image.freepik.com/free-vector/watercolor-background_87374-69.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {userId} - User Details Page
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
-            </CardActions>
+          <Card className={(classes.card, 'rounded-dynamic')}>
+            <CardMedia
+              className={classes.media}
+              image="https://image.freepik.com/free-vector/watercolor-background_87374-69.jpg"
+              title="Contemplative Reptile"
+            />
+
+            <CardContent className={classes.contentHeader}>
+              <div className={classes.rootAvatar}>
+                <StyledBadge
+                  overlap="circle"
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  variant="dot">
+                  <Avatar
+                    className={classes.large}
+                    alt="Remy Sharp"
+                    src="https://64.media.tumblr.com/f6514ff303f6a7e6cc2aea9783c0b676/tumblr_inline_pgpymmeSG41rl9zyi_400.gifv"
+                  />
+                </StyledBadge>
+              </div>
+              <Typography
+                className={classes.name}
+                gutterBottom
+                variant="h3"
+                component="h3">
+                {userId}
+              </Typography>
+            </CardContent>
           </Card>
         </Grid>
         <Grid item xs={4}>
-          <Card className={classes.card}>
+          <Card className={(classes.card, 'rounded-dynamic')}>
             <CardHeader
               title={
                 <Typography
@@ -115,7 +177,7 @@ const UserDetails = () => {
                   component="h5"
                   align="left"
                   style={{ fontWeight: 'bold' }}>
-                  Information
+                  Intro
                 </Typography>
               }
             />
@@ -123,106 +185,87 @@ const UserDetails = () => {
               {/* <Typography gutterBottom variant="h5" component="h2">
                 {userId} - User Details Page
               </Typography> */}
-              <Grid container wrap="nowrap" spacing={2}>
-                <Grid item>
-                  <AccessAlarm color="disabled" />
+
+              {Array.from(Array(5), (e, i) => (
+                <Grid key={i} container wrap="nowrap" spacing={2}>
+                  <Grid item>
+                    <AccessAlarm color="disabled" />
+                  </Grid>
+                  <Grid item xs>
+                    <Typography variant="subtitle1" component="h5" align="left">
+                      Cúp: 200
+                    </Typography>
+                  </Grid>
                 </Grid>
-                <Grid item xs>
-                  <Typography variant="subtitle1" component="h5" align="left">
-                    Cúp: 200
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid container wrap="nowrap" spacing={2}>
-                <Grid item>
-                  <AccessAlarm color="disabled" />
-                </Grid>
-                <Grid item xs>
-                  <Typography variant="subtitle1" component="h5" align="left">
-                    Cúp: 200
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid container wrap="nowrap" spacing={2}>
-                <Grid item>
-                  <AccessAlarm color="disabled" />
-                </Grid>
-                <Grid item xs>
-                  <Typography variant="subtitle1" component="h5" align="left">
-                    Cúp: 200
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid container wrap="nowrap" spacing={2}>
-                <Grid item>
-                  <AccessAlarm color="disabled" />
-                </Grid>
-                <Grid item xs>
-                  <Typography variant="subtitle1" component="h5" align="left">
-                    Cúp: 200
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid container wrap="nowrap" spacing={2}>
-                <Grid item>
-                  <AccessAlarm color="disabled" />
-                </Grid>
-                <Grid item xs>
-                  <Typography variant="subtitle1" component="h5" align="left">
-                    Cúp: 200
-                  </Typography>
-                </Grid>
-              </Grid>
+              ))}
             </CardContent>
           </Card>
         </Grid>
         <Grid item xs={8}>
-          <Paper className={classes.paper}>
-            {Array.from(Array(5), (e, i) => (
-              <div key={i}>
-                <Typography color="textSecondary" variant="body2" align="left">
-                  Jan 7, 2014
+          <Card className={(classes.card, 'rounded-dynamic')}>
+            <CardHeader
+              title={
+                <Typography
+                  variant="h3"
+                  component="h5"
+                  align="left"
+                  style={{ fontWeight: 'bold' }}>
+                  Matches
                 </Typography>
-                <Divider />
-                <Card className={classes.rootActivity}>
-                  <CardMedia
-                    className={classes.cover}
-                    image="https://image.freepik.com/free-vector/watercolor-background_87374-69.jpg"
-                    title="Live from space album cover"
-                  />
-                  <div className={classes.details}>
-                    <CardContent className={classes.content}>
-                      <Typography component="h5" variant="h5">
-                        Live From Space
-                      </Typography>
-                      <Typography variant="subtitle1" color="textSecondary">
-                        Mac Miller
-                      </Typography>
-                    </CardContent>
-                    <div className={classes.controls}>
-                      <IconButton aria-label="previous">
-                        {theme.direction === 'rtl' ? (
-                          <SkipNextIcon />
-                        ) : (
-                          <SkipPreviousIcon />
-                        )}
-                      </IconButton>
-                      <IconButton aria-label="play/pause">
-                        <PlayArrowIcon className={classes.playIcon} />
-                      </IconButton>
-                      <IconButton aria-label="next">
-                        {theme.direction === 'rtl' ? (
-                          <SkipPreviousIcon />
-                        ) : (
-                          <SkipNextIcon />
-                        )}
-                      </IconButton>
+              }
+            />
+            <CardContent className={classes.cardContent}>
+              {Array.from(Array(5), (e, i) => (
+                <div key={i}>
+                  <Typography
+                    color="textSecondary"
+                    variant="body2"
+                    align="left">
+                    Jan 7, 2014
+                  </Typography>
+                  <Divider />
+                  <Card className={classes.rootActivity}>
+                    <CardMedia
+                      className={classes.cover}
+                      image="https://image.freepik.com/free-vector/watercolor-background_87374-69.jpg"
+                      title="Live from space album cover"
+                    />
+                    <div className={classes.details}>
+                      <CardContent className={classes.content}>
+                        <Typography component="h5" variant="h5">
+                          Live From Space
+                        </Typography>
+                        <Typography variant="subtitle1" color="textSecondary">
+                          Mac Miller
+                        </Typography>
+                      </CardContent>
+                      <div className={classes.controls}>
+                        <IconButton aria-label="previous">
+                          {theme.direction === 'rtl' ? (
+                            <SkipNextIcon />
+                          ) : (
+                            <SkipPreviousIcon />
+                          )}
+                        </IconButton>
+                        <IconButton aria-label="play/pause">
+                          <PlayArrowIcon className={classes.playIcon} />
+                        </IconButton>
+                        <IconButton aria-label="next">
+                          {theme.direction === 'rtl' ? (
+                            <SkipPreviousIcon />
+                          ) : (
+                            <SkipNextIcon />
+                          )}
+                        </IconButton>
+                      </div>
                     </div>
-                  </div>
-                </Card>
-              </div>
-            ))}
-          </Paper>
+                  </Card>
+                </div>
+              ))}
+            </CardContent>
+          </Card>
+
+          <Paper className={classes.paper}></Paper>
         </Grid>
       </Grid>
     </div>
