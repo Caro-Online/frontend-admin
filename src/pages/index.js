2a879759c469e336df97b6d4ed2f2512670b3a58
Van Hiep <levanhiep.95.95@gmail.com>
2021-01-01 12:41:05 +0700

    add default page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    padding: theme.spacing(3),
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: '5rem',
+    fontFamily: 'Poppins',
+    fontWeight: 700,
+    color: 'lightpink',
+  },
+}));
 const Dashboard = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <div>Admin Page</div>
+    <div className={classes.root}>
+      <div className={classes.title}>Admin Page</div>
     </div>
   );
 };
